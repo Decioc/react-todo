06e83a5956652f2e6ac5d5e35231083aa3f3e098
decioc <decioc.kyndryl.com>
2025-04-03 16:57:46 -0400

    initial commit

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
+  const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
 
   return (
     <>
@@ -13,9 +22,10 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
-          />
-          <button className="add-list-button">Add Heading</button>
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
+           />
+           <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
